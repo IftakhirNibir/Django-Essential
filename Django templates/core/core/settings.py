3e71d2049a309d408b8ac1b9d5e53a23c3c10818
Iftakhir Mozumder Nibir <134167711+IftakhirNibir@users.noreply.github.com>
2023-12-17 00:31:15 +0600

    templates project update

--- Django templates/core/core/settings.py
+++ Django templates/core/core/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-aak0%xktgd(yiis43r@@7tzqb+o8w2(_qp2gw4snngn_8vv0*x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
